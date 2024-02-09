b44ab55a86b61767e67488371f959402fcff9023
LeonardoNascimento7 <zikaleo762@gmail.com>
2024-02-09 10:17:26 -0300

    commit

--- areasobretranstornos.php
+++ areasobretranstornos.php
@@ -1,24 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/psicologos.css">
     <title>Área sobre transtornos</title>
-    
+
 </head>
+
 <body>
-    <div class="navbar">
-        <div class="logo">
-            <!-- Insira sua logo aqui -->
-            <img src="img/logopadrao.png" alt="Logo">
+    <header class="navbar">
+        <div class="estrutura-logo">
+            <img src="img/logopadrao.png" alt="Logo" class="logo">
+            <span class="nome-empresa">POSITIVE MIND</span>
         </div>
-        <a href="#inicio">Início</a>
-        <a href="#comunidade">Comunidade</a>
-        <a href="#sair">Sair</a>
-        <div class="welcome">
-            Seja bem-vinda, Fulana <!-- Aqui você pode adicionar o nome da pessoa logada -->
-        </div>
+        <span class="nav-usuario">Bem-vindo (a),
+            <?php echo $_SESSION['usuario_nome']; ?>
+        </span>
+        <a href="comunidade.php" class="nav-home">Comunidade</a>
+    </header>
+    <h2>Sobre os Transtornos Psicológicos</h2>
+
+    <h4>O que é um transtorno mental?</h4>
+
+    <!-- Container Wrapper -->
+    <div class="container-wrapper">
+        <div class="container">Informações Aqui</div>
+        <div class="container">Informações Aqui</div>
+        <div class="container">Informações Aqui</div>
     </div>
+
+    <h3>
+        <span class="first-part">Você acha que precisa de ajuda? </span>
+        <a href="pagina_de_ajuda.html" class="second-part">Clique aqui para buscar ajuda.</a>
+    </h3>
 </body>
+
 </html>
