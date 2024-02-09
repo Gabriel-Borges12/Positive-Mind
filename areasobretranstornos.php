3f8be6dea0288dfeff02d25620b830d9be0df3a5
LeonardoNascimento7 <zikaleo762@gmail.com>
2024-02-09 08:35:30 -0300

    mjvjcvjhj

--- areasobretranstornos.php
+++ areasobretranstornos.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/psicologos.css">
     <title>Área sobre transtornos</title>
+    
 </head>
 <body>
-    
+    <div class="navbar">
+        <div class="logo">
+            <!-- Insira sua logo aqui -->
+            <img src="img/logopadrao.png" alt="Logo">
+        </div>
+        <a href="#inicio">Início</a>
+        <a href="#comunidade">Comunidade</a>
+        <a href="#sair">Sair</a>
+        <div class="welcome">
+            Seja bem-vinda, Fulana <!-- Aqui você pode adicionar o nome da pessoa logada -->
+        </div>
+    </div>
 </body>
 </html>
