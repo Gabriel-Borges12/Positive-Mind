7376b58fd00d336d6fe0b45020403ebb65758086
LeonardoNascimento7 <zikaleo762@gmail.com>
2024-02-19 09:55:50 -0300

    alterções leo

--- areasobretranstornos.php
+++ areasobretranstornos.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/psicologos.css">
     <title>Área sobre transtornos</title>
-
 </head>
 
 <body>
@@ -35,6 +34,46 @@
         <span class="first-part">Você acha que precisa de ajuda? </span>
         <a href="pagina_de_ajuda.html" class="second-part">Clique aqui para buscar ajuda.</a>
     </h3>
+
+    <footer class="main_footer container">
+        <div class="content">
+            <div class="colfooter">
+                <h4 class="titleFooter"> Menu</h4>
+                <ul>
+                    <li><a href="index.php" title="Página Inícial">Página Inícial</a></li>
+                    <li><a href="img/Termos.pdf" title="Termos de Uso" target="_blank">Termos de uso</a></li>
+                    <li><a href="comunidade.php" title="Comunidade">Comunidade</a></li>
+                    <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
+                </ul>
+            </div><!--Col Footer 1-->
+
+            <div class="colfooter">
+                <h4 class="titleFooter"> Contato</h4>
+                <p><i class="icon icon-mail"></i> positivemindhealther@gmail.com</p>
+                <p><i class="icon icon-phone"></i> 21 90000-0000</p>
+                <p><i class="icon icon-whatsapp"></i> 21 90000-0000</p>
+            </div><!--Col Footer 2-->
+
+            <div class="colfooter">
+                <h4 class="titleFooter"> Redes Sociais</h4>
+                <a href="#" class="botao"><span> <i class="icon icon-facebook">Início</i> </span></a>
+                <a href="https://www.instagram.com/positive_mindbrasil?igsh=dTF5NHRxbzQ2cTYw" class="botao">Insta<span>
+                        <i class="icon icon-instagram"></i> </span></a>
+                <a href="https://x.com/positivemindbr?s=21&t=NCfJWWvec0F2TialOt2tfg" class="botao">X<span> <i
+                            class="icon icon-twitter"></i> </span></a>
+                <a href="https://mail.google.com/mail/u/0/?ogbl#inbox?compose=GTvVlcSBnNccbNzvDzPznhqqpQfljcjPnpfflhHtsrkjKKLKpmMDtWgvkVCpgTtFwNfkFHfxZCDGg"
+                    class="botao">Gmail<span> <i class="icon icon-pinterest"></i> </span></a>
+            </div><!--Col Footer 3-->
+
+            <div class="clear"></div>
+        </div><!--Contant-->
+
+        <div class="main_footer_copy">
+            <p class="m-b-footer"> Positive Mind - 2024, todos os direitos reservados.</p>
+            <p class="by"><i class="icon icon-heart-3"></i> Desenvolvido por <a href="#" title="Healther">Healther</a>
+            </p>
+        </div>
+    </footer>
 </body>
 
 </html>
