57573d515b183cfa4d2983cad0f7a68ed781b052
Gabriel-Borges12 <borgesgs369@gmail.com>
2024-03-01 09:52:20 -0300

    alteração

--- areasobretranstornos.php
+++ areasobretranstornos.php
@@ -48,7 +48,7 @@
             <li><a href="index.php" title="Página Inícial">Página Inícial</a></li>
             <li><a href="img/Termos.pdf" title="Termos de Uso" target="_blank">Termos de uso</a></li>
             <li><a href="comunidade.php" title="Comunidade">Comunidade</a></li>
-            <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
+            <li><a href="fale_conosco1" title="Fale Conosco">Fale Conosco</a></li>
 
         </ul>
 
