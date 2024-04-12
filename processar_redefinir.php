dff9982a75929cbcb22f63ed232165d9799839aa
Gabriel-Borges12 <borgesgs369@gmail.com>
2024-04-12 12:09:23 -0300

    alterações

--- processar_redefinir.php
+++ processar_redefinir.php
@@ -2,30 +2,33 @@
 include 'conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Receba o email do formulário
+    // Receba o email e a nova senha do formulário
     $usuario_email = $_POST['usuario_email'];
-    $newPassword = $_POST['new_password'];
+    $usuario_senha = $_POST['usuario_senha'];
 
     // Criptografe a nova senha
-    $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+    $hashedPassword = password_hash($usuario_senha, PASSWORD_DEFAULT);
 
     // Execute uma consulta para atualizar a senha criptografada
     $sql = "UPDATE usuarios SET usuario_senha = ? WHERE usuario_email = ?";
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $hashedPassword, $email);
+    $stmt->bind_param("ss", $hashedPassword, $usuario_email);
 
     if ($stmt->execute()) {
-        //header("Location: javascript:alert(\"Email enviado com sucesso!\");location.href=\"index.php\";");
-        echo '<script>alert("Registro atualizado com sucesso!");</script>';
+        echo '<script>alert("Senha redefinida com sucesso!");</script>';
         echo '<script>
                 setTimeout(function(){
                     window.location.href = "login.php";
-                }, 1); 
+                }, 1000); 
               </script>';
     } else {
         echo "Erro ao redefinir a senha: " . $conn->error;
     }
 
+    // Feche a instrução preparada
+    $stmt->close();
+
     // Feche a conexão com o banco de dados
     $conn->close();
 }
+?>
